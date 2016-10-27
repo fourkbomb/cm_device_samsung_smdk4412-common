3c855977920a491591659ec0c36880ada030ae78
Simon Shields <keepcalm444@gmail.com>
2016-10-28 01:27:49 +1100

    updates for mainline linux
    Change-Id: If5bf7a3869870106de00bcc3ddcba7fcf769424c

--- libhealthd/healthd_board_exynos4.cpp
+++ libhealthd/healthd_board_exynos4.cpp
@@ -20,14 +20,14 @@
 void
 healthd_board_init(struct healthd_config *config)
 {
-    config->batteryCapacityPath    = "/sys/class/power_supply/battery/capacity";
-    config->batteryStatusPath      = "/sys/class/power_supply/battery/status";
-    config->batteryVoltagePath     = "/sys/class/power_supply/battery/voltage_now";
-    config->batteryCurrentNowPath  = "/sys/class/power_supply/battery/current_now";
-    config->batteryPresentPath     = "/sys/class/power_supply/battery/present";
-    config->batteryHealthPath      = "/sys/class/power_supply/battery/health";
-    config->batteryTemperaturePath = "/sys/class/power_supply/battery/temp";
-    config->batteryTechnologyPath  = "/sys/class/power_supply/battery/technology";
+    config->batteryCapacityPath    = "/sys/class/power_supply/max170xx_battery/capacity";
+    config->batteryStatusPath      = "/sys/class/power_supply/max77693-charger/status";
+    config->batteryVoltagePath     = "/sys/class/power_supply/max170xx_battery/voltage_now";
+    config->batteryCurrentNowPath  = "/sys/class/power_supply/max77593-charger/current_now";
+    config->batteryPresentPath     = "/sys/class/power_supply/max170xx_battery/present";
+    config->batteryHealthPath      = "/sys/class/power_supply/max170xx_battery/health";
+    config->batteryTemperaturePath = "/sys/class/power_supply/max170xx_battery/temp";
+    config->batteryTechnologyPath  = "/sys/class/power_supply/max170xx_battery/technology";
     // others use defaults
 }
 
